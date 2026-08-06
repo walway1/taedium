731df895745ec636f2c714e628937f701fa884e0
walway1 <denizgamingyt95@gmail.com>
2026-08-07 00:50:10 +0300

    Added incredibly sloppy support for the letter "a".

--- src/i686/boot/kernel.c
+++ src/i686/boot/kernel.c
@@ -46,28 +46,29 @@ size_t strlen(const char* str)
 #define VGA_HEIGHT  25
 #define VGA_MEMORY  0xB8000 
 
-size_t term_row;
-size_t term_column;
-uint8_t term_color;
+size_t trow;
+size_t tcol;
+uint8_t tcolor;
 uint16_t* term_buffer = (uint16_t*)VGA_MEMORY;
+uint8_t read_port;
 
 void term_init(void) 
 {
-	term_row = 0;
-	term_column = 0;
-	term_color = vga_entry_color(VGA_WHITE, VGA_BLACK);
+	trow = 0;
+	tcol = 0;
+	tcolor = vga_entry_color(VGA_WHITE, VGA_BLACK);
 	
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
-			term_buffer[index] = vga_entry(' ', term_color);
+			term_buffer[index] = vga_entry(' ', tcolor);
 		}
 	}
 }
 
 void term_setcolor(uint8_t color) 
 {
-	term_color = color;
+	tcolor = color;
 }
 
 void term_putentryat(char c, uint8_t color, size_t x, size_t y) 
@@ -78,28 +79,17 @@ void term_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void term_putchar(char c) 
 {
-	term_putentryat(c, term_color, term_column, term_row);
-	if (++term_column == VGA_WIDTH) {
-		term_column = 0;
-		if (++term_row == VGA_HEIGHT)
-			term_row = 0;
+	term_putentryat(c, tcolor, tcol, trow);
+	if (++tcol == VGA_WIDTH) {
+		tcol = 0;
+		if (++trow == VGA_HEIGHT)
+			trow = 0;
 	}
-}
-
-void term_write(const char* data, size_t size) 
-{
-	for (size_t i = 0; i < size; i++)
-		term_putchar(data[i]);
-}
-
-void term_writestring(const char* data) 
-{
-	term_write(data, strlen(data));
 }
 static inline void outb(uint16_t port, uint8_t val) {
     asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
-void term_update_cursor(int row, int col) {
+void term_set_cursor(int row, int col) {
     uint16_t cursor_pos = row * 80 + col;
 
     outb(0x3D4, 0x0F);
@@ -107,6 +97,19 @@ void term_update_cursor(int row, int col) {
     outb(0x3D4, 0x0E);
     outb(0x3D5, (uint8_t)((cursor_pos >> 8) & 0xFF));
 }
+void term_write(const char* data, size_t size) 
+{
+	for (size_t i = 0; i < size; i++)
+		term_putchar(data[i]);
+
+}
+
+void term_writestring(const char* data) 
+{
+	term_write(data, strlen(data));
+	term_set_cursor(trow, tcol + strlen(data)/2);
+}
+
 static inline uint8_t asm_in(uint16_t port) {
 	uint8_t ret;
 	asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
@@ -127,38 +130,38 @@ void term_print_port(uint16_t port) {
 	convert_hex(val, buf);
 	term_write(buf, strlen(buf));
 }
+
+void hlt(void) {
+	__asm__ volatile ("hlt");
+}
 void kernel_main(void) 
 {
 	*(volatile uint32_t*)0xB80BC = 0x0A4F0F5B;
     *(volatile uint32_t*)0xB80C0 = 0x0F5D0A4B;
-	term_row = 2;
-	term_column = 0;
+	trow = 2;
+	tcol = 0;
 	term_setcolor(vga_entry_color(VGA_LIGHT_GREY, VGA_BLACK));
 	term_writestring("-------------------------");
-	term_column = 24;
+	tcol = 24;
 	term_setcolor(vga_entry_color(VGA_WHITE, VGA_BLACK));
 	term_writestring("Welcome to Taedium v0.0.2 Alpha");
-	term_column = 55;
+	tcol = 55;
 	term_setcolor(vga_entry_color(VGA_LIGHT_GREY, VGA_BLACK));
 	term_writestring("--------------------------");
-	term_row = 3;
-	term_column = 0;
+	trow = 3;
+	tcol = 0;
 	term_setcolor(vga_entry_color(VGA_LIGHT_GREEN, VGA_BLACK));
-	term_update_cursor(3, 8);
 	term_writestring("Taedium>");
 	term_setcolor(vga_entry_color(VGA_WHITE, VGA_BLACK));
-	term_print_port(0x60);
-	size_t loop;
-	while (true) {
-		if (loop < 10000) {
-			loop += 1;
-		}
-		else {
-			term_row = 3;
-			term_column = 8;
-			term_print_port(0x60);
-			loop = 0;
-		}
-	}
-		
+	term_set_cursor(3, 8);
+
+while (true) {
+    for (volatile uint32_t i = 0; i < 49000000; i++) {
+
+    }
+
+    if (asm_in(0x60) == 0x1E) {
+        term_writestring("a");
+    }
+}
 }
