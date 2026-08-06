8141f3d619a1c0df58bd85670f9bdb34e47d20d8
walway1 <denizgamingyt95@gmail.com>
2026-08-07 01:31:58 +0300

    Improved testiso-rh

--- testiso-rh.sh
+++ testiso-rh.sh
@@ -1,7 +1,8 @@
-read -p "WARNING! This WILL overwrite whatever data is on /dev/sda IRREVERSIBLY! Continue? [y/N]" yn
+read -p "Please type the path of the device the ISO will be burnt to (e.g. /dev/sda" device
+read -p "WARNING! This WILL overwrite whatever data is on $(device) IRREVERSIBLY! Continue? [y/N]" yn
 case $yn in 
 	[Yy]* )
-		make clean && make -j$(nproc) && ./makeiso.sh && sudo dd if=taedium.iso of=/dev/sda conv=fsync bs=4K status=progress
+		make clean && make -j$(nproc) && ./makeiso.sh && sudo dd if=taedium.iso of=$(device) conv=fsync bs=4K status=progress
 	;;
 	[Nn]* )
 		exit 1
